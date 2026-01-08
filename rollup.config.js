0002917b5ebb762f8473746b4f08885b9aea9ab4
老王杂谈说 <djhui@live.cn>
2026-01-08 14:21:00 +0800

    "rename-to-ha-reachy-mini-card"

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'src/reachy-mini-3d-card.js',
   output: {
-    file: 'reachy-mini-3d-card.js',
+    file: 'ha-reachy-mini-card.js',
     format: 'iife',
     name: 'ReachyMini3DCard',
     sourcemap: true,
